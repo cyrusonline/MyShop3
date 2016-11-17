aa27b1276c98e259d48ece0475b336962e8dc332
C.K. Chan <chanchikin@ied.edu.hk>
2016-11-17 23:38:51 +0800

    BaseLiveService

---
+++ app/src/live/java/com/example/ck/myshop2/live/BaseLiveService.java
@@ -0,0 +1,11 @@
+package com.example.ck.myshop2.live;
+
+/**
+ * Created by chanchikin on 11/17/2016.
+ */
+
+public class BaseLiveService {
+    
+}
+
+
--- app/src/main/java/com/example/ck/myshop2/activities/RegisterActivity.java
+++ app/src/main/java/com/example/ck/myshop2/activities/RegisterActivity.java
@@ -1,5 +1,6 @@
 package com.example.ck.myshop2.activities;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -8,6 +9,7 @@ import android.widget.EditText;
 import android.widget.LinearLayout;
 
 import com.example.ck.myshop2.R;
+import com.example.ck.myshop2.services.AccountServices;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -27,12 +29,25 @@ public class RegisterActivity extends BaseActivity{
     @BindView(R.id.activity_register_userEmail)
     EditText userEmail;
 
+    @BindView(R.id.activity_register_userName)
+    EditText userName;
+
+    @BindView(R.id.activity_login_registerButton)
+    Button registerButton;
+
+    ProgressDialog mProgressDialog;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         ButterKnife.bind(this);
         linearLayout.setBackgroundResource(R.drawable.background_screen_two);
+        mProgressDialog = new ProgressDialog(this);
+        mProgressDialog.setTitle("Loading...");
+        mProgressDialog.setMessage("Attempting to register account");
+        mProgressDialog.setCancelable(false);
+
     }
 
     @OnClick(R.id.activity_register_loginButton)
@@ -40,4 +55,11 @@ public class RegisterActivity extends BaseActivity{
         startActivity(new Intent(this,LoginActivity.class));
         finish();
     }
+
+    @OnClick(R.id.activity_register_registerButton)
+    public void setRegisterButton(){
+        bus.post(new AccountServices.RegisterUserRequest(userName.getText().toString(),userEmail.getText().toString(),mProgressDialog));
+
+    }
+
 }
